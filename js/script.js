3ab29e47ed2cd8de59637610cd29b1364f7787b5
Alex Mihov <alexmihov.as@gmail.com>
2024-12-18 14:52:02 +0100

    feat valori da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Consegna:
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+// Consigli del giorno:
+
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”;
+
+// Dopo esserci concentrati sul far funzionare le cose e averle testate,
+// ragioniamo  sul possibile refactoring, quindi ottimizzazione possibile,
+// sia del codice come performance, ma anche, se non soprattutto come leggibilità e mantenibilità. :genio_uomo:
+
+// stampiamo i valori da 1 a 100 
+for (let i = 0; i <= 100; i++) {
+    console.log("il valore di i è: ", i)
+
+    // SE il numero è un multiplo di 3 stampo "Fizz"
+    if (i % 3 === 0) {
+        console.log("il valore di i è: ", "Fizz")
+    }
+
+}
+    
+    // ALTRIMENTI SE il numero è un multiplo di 5 stampo "Buzz"
+    // ALTRIMENTI SE il numero è un multiplo di 3 e 5 stampo "FizzBuzz"
+    // ALTRIMENTI stampo i numeri normalmente
